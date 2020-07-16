42d634dec5cf4a49a971dca3889b91f7449288c7
Jakub Jaskuła <jjcreator8@gmail.com>
2020-07-16 10:40:05 +0200

    Updates

---
+++ precache-manifest.9fc69169fae871b4d4b3a9cfd56d7b5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43bc910a1b7f861bfe510963bfd2e1ae",
+    "url": "/clocks/index.html"
+  },
+  {
+    "revision": "8a8605b3bffb64b29f3e",
+    "url": "/clocks/static/css/main.a5d09c10.chunk.css"
+  },
+  {
+    "revision": "abf38b48f6692f1ca890",
+    "url": "/clocks/static/js/2.aef8f6f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clocks/static/js/2.aef8f6f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a8605b3bffb64b29f3e",
+    "url": "/clocks/static/js/main.120da36d.chunk.js"
+  },
+  {
+    "revision": "77c271a7769e9fe8fcc5",
+    "url": "/clocks/static/js/runtime-main.94dd42d8.js"
+  }
+]);
